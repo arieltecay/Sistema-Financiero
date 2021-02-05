485bdc2c914da047cd5e3cd059e0188899954bd7
arieltecay <arieltecay@gmail.com>
2021-02-05 11:13:16 -0300

    Proteccion de la ruta del servidor

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import movimientosRoute from './routes/movimientos.js'
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
@@ -12,10 +14,9 @@ app.use(cors());
 
 app.use('/movimientos', movimientosRoute);
 
-const CONNECTION_URL = 'mongodb+srv://arieltecay:arieltecay123@cluster0.tlbsv.mongodb.net/0Cluster?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.env(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`server Runing on Port ${PORT}`)))
     .catch((error) => console.log(error.message));
 
